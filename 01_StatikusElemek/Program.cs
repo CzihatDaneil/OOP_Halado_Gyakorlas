7cde6312820b210ae78ffbead4519ea6ca6b1e3b
12d <12d@202-2-3>
2026-09-24 10:47:52 +0200

    1.feladat

--- 01_StatikusElemek/Program.cs
+++ 01_StatikusElemek/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using _01_StatikusElemek;
+Console.WriteLine($"100Eur to HUF:{ValutaValto.EurToHuf(100)}");
+Console.WriteLine($"250USD to HUF:{ValutaValto.UsdToHuf(250)}");
+Console.WriteLine($"Össze átváltás:{ValutaValto.GetOsszesAtvaltas()}");
