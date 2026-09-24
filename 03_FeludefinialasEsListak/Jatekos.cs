45d959f826e712bc6b6a71685c1674ba260d42a3
12d <12d@202-2-3>
2026-09-24 11:16:54 +0200

    1. jatekos

---
+++ 03_FeludefinialasEsListak/Jatekos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_FeludefinialasEsListak
+{
+    internal class Jatekos
+    {
+        private string nev;
+        private int szint;
+        public string Nev { get; set; }
+        public int Szint
+        {
+            get
+            {
+                return szint;
+            }
+            set
+            {
+                if (value < 1)
+                {
+                    szint = 1;
+                }
+                else if (value > 100)
+                {
+                    szint = 100;
+                }
+                else
+                {
+                    szint = value;
+                }
+            }
+        }
+    }  
+}
